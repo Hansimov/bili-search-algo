1db12f9b8bb917ba096429e1a404d6b458e45ad1
Hansimov <591172499@qq.com>
2024-09-22 20:51:29 +0800

    :pencil: [Config] create envs, envs.json and secrets

---
+++ configs/envs.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+from tclogger import OSEnver
+
+REPO_ROOT = Path(__file__).parents[1]
+CONFIGS_ROOT = REPO_ROOT / "configs"
+ENVS_PATH = CONFIGS_ROOT / "envs.json"
+LOG_ROOT = REPO_ROOT / "logs"
+
+ENVS_ENVER = OSEnver(ENVS_PATH)
+LOG_ENVS = ENVS_ENVER["logs"]
+
+SECRETS_PATH = CONFIGS_ROOT / "secrets.json"
+SECRETS = OSEnver(SECRETS_PATH)
+MONGO_ENVS = SECRETS["mongo"]
+ELASTIC_ENVS = SECRETS["elastic"]
