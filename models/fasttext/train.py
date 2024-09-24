0606eaef8ab489344d491990e2c25fe36fa5ea9d
Hansimov <591172499@qq.com>
2024-09-24 18:29:42 +0800

    :zap: [Enhance] FasttextModelTrainer: load model before data, and prettify test results

--- models/fasttext/train.py
+++ models/fasttext/train.py
@@ -1,9 +1,10 @@
 import argparse
 import sys
 
+from copy import deepcopy
 from gensim.models import FastText, KeyedVectors
 from pathlib import Path
-from tclogger import logger, logstr
+from tclogger import logger, logstr, dict_to_str
 from typing import Union
 
 from models.fasttext.data import VideosTagsDataLoader
@@ -13,18 +14,7 @@ class FasttextModelTrainer:
     def __init__(self):
         pass
 
-    def load_data(self, max_count: int = None):
-        logger.note(f"> Loading data: [{logstr.mesg(max_count)}]")
-        self.data_loader = VideosTagsDataLoader(
-            collection="videos_tags",
-            max_count=max_count,
-            iter_val="tag_list",
-            iter_log=True,
-        )
-        logger.file(f"  * from class {self.data_loader.__class__.__name__}")
-        logger.success(f"  ✓ data loaded")
-
-    def load_model(self, model_path: Union[str, Path], use_local: bool = True):
+    def load_model(self, model_path: Union[str, Path], use_local_model: bool = True):
         self.model_path = Path(model_path)
         self.model = None
         self.is_model_trained = False
@@ -35,14 +25,31 @@ class FasttextModelTrainer:
             self.is_model_trained = True
             return
 
-        if self.model_path.exists() and use_local:
+        if self.model_path.exists() and use_local_model:
+            logger.file(f"  * load from local: [{self.model_path}]")
             self.model = FastText.load(str(self.model_path))
             self.is_model_trained = True
-            logger.file(f"  * load from local: [{self.model_path}]")
         else:
-            self.model = FastText(vector_size=100, window=3, min_count=1)
+            self.model = FastText(vector_size=100, window=5, min_count=1)
             self.is_model_trained = False
             logger.success("  ✓ new model created")
+
+    def load_data(self, max_count: int = None):
+        logger.note(f"> Loading data: [{logstr.mesg(max_count)}]")
+        if self.is_model_trained:
+            logger.file("  * model already trained, skip load_data()")
+        else:
+            self.data_loader_params = {
+                "collection": "videos_tags",
+                "max_count": max_count,
+                "iter_val": "tag_list",
+                "iter_log": True,
+            }
+            self.data_loader = VideosTagsDataLoader(
+                **self.data_loader_params,
+            )
+            logger.success("  ✓ data loader created")
+            logger.mesg(dict_to_str(self.data_loader_params), indent=4)
 
     def build_vocab(self, skip_trained: bool = True):
         logger.note("> Building vocab:")
@@ -51,7 +58,7 @@ class FasttextModelTrainer:
         else:
             self.model.build_vocab(corpus_iterable=self.data_loader)
             # self.model_total_words = self.model.corpus_total_words
-            logger.success("  ✓ vocab built")
+            logger.success(f"  ✓ vocab built")
 
     def train(
         self, epochs: int = 5, skip_trained: bool = True, overwrite: bool = False
@@ -60,13 +67,14 @@ class FasttextModelTrainer:
         if self.is_model_trained and skip_trained:
             logger.file("  * model already trained, skip train()")
         else:
+            self.data_loader.iter_epochs = epochs
             self.model.train(
                 corpus_iterable=self.data_loader,
                 total_examples=self.data_loader.max_count,
                 # total_words=self.model_total_words,
                 epochs=epochs,
             )
-            logger.success("  ✓ model trained")
+            logger.success(f"  ✓ model trained")
 
             if self.model_path.exists() and not overwrite:
                 logger.file(f"> Skip saving existed model")
@@ -84,22 +92,23 @@ class FasttextModelTrainer:
             logger.warn("  × No test words provided")
         else:
             for word in test_words:
-                similar_words = self.model.wv.most_similar(word)[:6]
+                results = self.model.wv.most_similar(word)[:6]
                 logger.mesg(f"  * [{logstr.file(word)}]:")
-                for similar_word in similar_words:
-                    logger.success(f"    * {similar_word}")
+                for result in results:
+                    res_word, res_score = result
+                    logger.success(f"    * {res_score:>.4f}: {res_word}")
 
     def run(
         self,
         model_path: Union[str, Path],
         max_count: int = 10000,
         epochs: int = 5,
-        use_local: bool = True,
+        use_local_model: bool = True,
         skip_trained: bool = True,
         overwrite: bool = True,
     ):
+        self.load_model(model_path, use_local_model=use_local_model)
         self.load_data(max_count=max_count)
-        self.load_model(model_path, use_local=use_local)
         self.build_vocab(skip_trained=skip_trained)
         self.train(epochs=epochs, skip_trained=skip_trained, overwrite=overwrite)
 
@@ -119,19 +128,19 @@ if __name__ == "__main__":
 
     trainer_params = {
         "model_path": model_path,
-        "max_count": 1000000,
-        "epochs": 5,
+        "max_count": 10000000,
+        "epochs": 10,
     }
 
     if args.test_only:
         extra_params = {
-            "use_local": True,
+            "use_local_model": True,
             "skip_trained": True,
             "overwrite": False,
         }
     else:
         extra_params = {
-            "use_local": False,
+            "use_local_model": False,
             "skip_trained": False,
             "overwrite": True,
         }
@@ -141,7 +150,7 @@ if __name__ == "__main__":
         **extra_params,
     )
 
-    test_words = ["香港", "搞笑", "萌宠", "GTA"]
+    test_words = ["上海", "搞笑", "萌宠", "GTA", "高数"]
     trainer.test(test_words)
 
     # python -m models.fasttext.train
