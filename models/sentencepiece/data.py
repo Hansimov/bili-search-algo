a3b5931e64277feac237f8951778144df099a376
Hansimov <591172499@qq.com>
2024-11-09 17:16:25 +0800

    :zap: [Enhance] SentencesDataLoader: use VideosTagsAggregator to simplify doc_batch, and prettify logging

--- models/sentencepiece/data.py
+++ models/sentencepiece/data.py
@@ -58,7 +58,6 @@ class VideosTagsAggregator:
 
 
 class SentencesDataloader:
-
     def __init__(
         self,
         videos_collect_name: str = "videos",
@@ -74,7 +73,6 @@ class SentencesDataloader:
         self.max_batch = max_batch
         self.iter_val = iter_val
         self.iter_epochs = iter_epochs
-        self.iter_epoch = 0
         self.init_mongo()
         self.init_progress_bars()
 
@@ -82,80 +80,68 @@ class SentencesDataloader:
         self.epoch_bar = TCLogbar(head=logstr.note("> Epoch:"))
         self.batch_bar = TCLogbar(head=logstr.note("  * Batch:"))
         self.sample_bar = TCLogbar(head=logstr.note("  * Sample:"))
-        TCLogbarGroup([self.epoch_bar, self.batch_bar, self.sample_bar])
+        TCLogbarGroup(
+            [self.epoch_bar, self.batch_bar, self.sample_bar], show_at_init=True
+        )
 
     def init_mongo(self):
-        self.mongo = MongoOperator(
-            MONGO_ENVS, connect_msg=f"from {self.__class__.__name__}", indent=2
-        )
-        self.videos_collect = self.mongo.db[self.videos_collect_name]
-        self.videos_cursor = self.mongo.get_cursor(self.videos_collect_name)
-        self.store_cursor()
-
-    def store_cursor(self):
-        self.videos_cursor_unevaluated = self.videos_cursor.clone()
+        self.aggregator = VideosTagsAggregator()
+        self.cursor = self.aggregator.cursor
 
     def restore_cursor(self):
-        self.videos_cursor = self.videos_cursor_unevaluated.clone()
+        self.aggregator.init_cursor()
+        self.cursor = self.aggregator.cursor
+
+    def __epoch_start__(self):
+        self.epoch_bar.total = self.iter_epochs or 1
+        self.epoch_bar.update(0, flush=True)
+        self.batch_bar.total = (
+            int(self.aggregator.videos_estimated_count / self.batch_size) + 1
+        )
 
     def __epoch_end__(self):
         self.epoch_bar.update(increment=1)
         self.batch_bar.reset()
         self.sample_bar.reset()
+        self.restore_cursor()
 
     def doc_batch(self) -> Generator[dict, None, None]:
         while True:
-            videos_batch = []
-            videos_count = 0
-            for doc in self.videos_cursor:
-                videos_batch.append(doc)
-                videos_count += 1
-                if videos_count >= self.batch_size:
-                    videos_count = 0
+            res = []
+            for idx, doc in enumerate(self.cursor):
+                res.append(doc)
+                if (idx + 1) % self.batch_size == 0:
+                    self.batch_bar.update(increment=1)
                     break
-            if not videos_batch:
+            if not res:
                 break
-            bvids = [doc["bvid"] for doc in videos_batch]
-            filter = {"bvid": {"$in": bvids}}
-            projection = {"bvid": 1, "tags": 1, "region_tags": 1}
-            tags_cursor = self.mongo.db[self.tags_collect_name].find(filter, projection)
-            tags_dict = {doc["bvid"]: doc for doc in tags_cursor}
-            res = {}
-            self.sample_bar.total = len(videos_batch)
-            for doc in videos_batch:
-                bvid = doc["bvid"]
-                tags_doc = tags_dict.get(bvid, {})
-                combined_doc = {**doc, **tags_doc}
-                res[bvid] = combined_doc
-                self.sample_bar.update(increment=1)
             yield res
 
     def doc_to_sentence(self, doc: dict) -> str:
-        title = doc.get("title", "")
-        desc = doc.get("desc", "")
-        author = doc.get("owner", {}).get("name", "")
-        region_tags = doc.get("region_tags", "")
-        tags = doc.get("tags", "")
+        title = dict_get(doc, "title", "")
+        desc = dict_get(doc, "desc", "")
+        author = dict_get(doc, "owner.name", "")
+        region_tags = dict_get(doc, "tagger.region_tags", "")
+        tags = dict_get(doc, "tagger.tags", "")
         sentence = f"Title: {title}. Desc: {desc}. Author: {author}. Tags: {region_tags}, {tags}"
         return sentence
 
     def __iter__(self):
-        self.epoch_bar.total = self.iter_epochs or 1
-        self.batch_bar.total = (
-            int(self.videos_collect.estimated_document_count() / self.batch_size) + 1
-        )
+        self.__epoch_start__()
         for batch_idx, batch in enumerate(self.doc_batch()):
             if self.max_batch is not None and batch_idx >= self.max_batch:
                 break
-            for bvid, doc in batch.items():
+            self.sample_bar.total = len(batch)
+            for doc in batch:
                 if self.iter_val == "sentence":
                     res = self.doc_to_sentence(doc)
                 else:
                     res = doc
+                self.sample_bar.update(increment=1)
                 yield res
             self.sample_bar.reset()
-            self.batch_bar.update(increment=1)
-        self.__epoch_end__()
+        if self.iter_epochs and self.iter_epochs > 1:
+            self.__epoch_end__()
 
 
 if __name__ == "__main__":
