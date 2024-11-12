2df4517bcf4da5daa3996f5d7d07b200aed6213a
Hansimov <591172499@qq.com>
2024-11-12 23:31:36 +0800

    :zap: [Enhance] DocSentenceConverter: better concat fields in doc_to_sentence, replace non-word with whitespaces, and merge whitespaces

--- models/sentencepiece/convert.py
+++ models/sentencepiece/convert.py
@@ -1,4 +1,5 @@
 import re
+import string
 
 from tclogger import dict_get
 
@@ -7,57 +8,72 @@ class DocSentenceConverter:
     """
     GB2312 编码表:
         - https://www.toolhelper.cn/Encoding/GB2312
-        - A1A0~A3FE (JP), A6A0~A9FE (CN)
+        - A1A0~A3FE (JP), A6A0~A9FE (ZH)
     CJK Unicode Tables:
         - https://www.khngai.com/chinese/charmap/tbluni.php
-        - 0x4E00~0x9FFF (CN)
+        - 0x4E00~0x9FFF (ZH)
     Unicode Kanji Table:
         - http://www.rikai.com/library/kanjitables/kanji_codes.unicode.shtml
         - 0x3040~0x30FF (JP)
     """
 
-    RE_CJK = r"[\u4E00-\u9FFF\u3040-\u30FF]"
-    RE_CJK_SPACE = rf"(?<={RE_CJK})\s+(?={RE_CJK})"
+    RE_CJK = r"\u4E00-\u9FFF\u3040-\u30FF"
+    RE_EN = r"0-9a-zA-Z"
+    RE_DASH = r"\-\_\."
+
+    RE_CJK_SPACE = rf"(?<=[{RE_CJK}])\s+(?=[{RE_CJK}])"
+
+    RE_NON_WORD = rf"[^({RE_CJK})|({RE_EN})|({RE_DASH})]"
 
     def doc_to_sentence(self, doc: dict) -> str:
         author = dict_get(doc, "owner.name", "")
-        author_str = f"{author}:" if author else ""
+        author_str = f"{author}" if author else ""
 
         title = dict_get(doc, "title", "")
-        title_str = f"{title}." if title else ""
+        title_str = f"{title}" if title else ""
 
         desc = dict_get(doc, "desc", "")
-        desc_str = f"{desc}." if desc else ""
+        desc_str = f"{desc}" if desc else ""
 
         rtags = dict_get(doc, "rtags", "")
         tags = dict_get(doc, "tags", "")
-        tags_str = f"{rtags}, {tags}." if tags else f"{rtags}."
-        tags_str = tags_str.replace(" ", "")
+        tags_str = f"{rtags}, {tags}" if tags else f"{rtags}"
 
-        sentence = f"{author_str}{title_str}{desc_str}{tags_str}"
+        sentence = f"{author_str} | {title_str} | {desc_str} | {tags_str}"
         return sentence
 
-    def lower_and_strip(self, sentence: str) -> str:
-        return sentence.lower().strip()
+    def lower(self, sentence: str) -> str:
+        return sentence.lower()
 
     def remove_whitespaces_among_cjk(self, sentence: str) -> str:
         return re.sub(self.RE_CJK_SPACE, "", sentence)
 
+    def replace_non_word_with_whitespaces(self, sentence: str) -> str:
+        return re.sub(self.RE_NON_WORD, " ", sentence)
+
+    def merge_whitespaces(self, sentence: str) -> str:
+        return re.sub(r"\s+", " ", sentence).strip()
+
+    def convert_sentence(self, sentence: str) -> str:
+        sentence = self.lower(sentence)
+        sentence = self.remove_whitespaces_among_cjk(sentence)
+        sentence = self.replace_non_word_with_whitespaces(sentence)
+        sentence = self.merge_whitespaces(sentence)
+        return sentence
+
     def convert(self, doc: dict) -> str:
         sentence = self.doc_to_sentence(doc)
-        sentence = self.lower_and_strip(sentence)
-        sentence = self.remove_whitespaces_among_cjk(sentence)
+        sentence = self.convert_sentence(sentence)
         return sentence
 
 
 if __name__ == "__main__":
     from tclogger import logger
 
-    sentence = " 这是 一段 中文。这是日语：これは 日本語 です。 Here is some English. "
+    sentence = " 这是 一段 中文。这是日语：これは 日本語 です。 Here is some English. https://www.google.com"
     logger.note(sentence)
     converter = DocSentenceConverter()
-    sentence = converter.lower_and_strip(sentence)
-    sentence = converter.remove_whitespaces_among_cjk(sentence)
+    sentence = converter.convert_sentence(sentence)
     logger.success(sentence)
 
     # python -m models.sentencepiece.convert
