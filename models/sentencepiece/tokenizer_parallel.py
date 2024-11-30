51a7e4f01be7b24c25d3c403e68f17cdcb7a54e5
Hansimov <591172499@qq.com>
2024-12-01 01:36:03 +0800

    :gem: [Feature] ParallelSentenceFullTokenizer: moduralize test_parallel and test_parallel_iter, and add ArgParser for these two tests

--- models/sentencepiece/tokenizer_parallel.py
+++ models/sentencepiece/tokenizer_parallel.py
@@ -1,4 +1,6 @@
+import argparse
 import multiprocessing as mp
+import sys
 
 from collections.abc import Iterable, Generator
 from pathlib import Path
@@ -123,24 +125,48 @@ class ParallelSentenceFullTokenizer:
             p.join()
 
 
+def test_parallel(tokenizer: ParallelSentenceFullTokenizer):
+    logger.note(f"> tokenize_list: {logstr.mesg(brk(tokenizer.workers_num))}")
+    sentences = TEST_SENTENCES
+    results = tokenizer.tokenize_list(sentences)
+    for result in results:
+        logger.mesg(f"  * {result['string']}")
+    tokenizer.terminate()
+
+
+def test_parallel_iter(tokenizer: ParallelSentenceFullTokenizer):
+    logger.note(f"> tokenize_iter: {logstr.mesg(brk(tokenizer.workers_num))}")
+    sentences = TEST_SENTENCES
+    for results in tokenizer.tokenize_iter(sentences):
+        for result in results:
+            logger.mesg(f"  * {result['string']}")
+    tokenizer.terminate()
+
+
+class ArgParser(argparse.ArgumentParser):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.add_argument("-i", "--iter", action="store_true")
+
+    def parse_args(self):
+        self.args, self.unknown_args = self.parse_known_args(sys.argv[1:])
+        return self.args
+
+
 if __name__ == "__main__":
     from models.sentencepiece.test import TEST_SENTENCES
 
+    args = ArgParser().parse_args()
     model_prefix = "sp_400k_merged"
     model_path = str(Path(__file__).parents[2] / f"{model_prefix}.model")
     tokenizer = ParallelSentenceFullTokenizer(
         model_path, drop_non_word=True, verbose=False, workers_num=8
     )
 
-    logger.note(
-        f"> Parallel Tokenizing with workers: {logstr.mesg(brk(tokenizer.workers_num))}"
-    )
-    sentences = TEST_SENTENCES
-    results = tokenizer.tokenize(sentences)
-
-    for sentence, result in zip(sentences, results):
-        logger.mesg(f"  * {result['string']}")
-
-    tokenizer.terminate()
+    if args.iter:
+        test_parallel_iter(tokenizer)
+    else:
+        test_parallel(tokenizer)
 
     # python -m models.sentencepiece.tokenizer_parallel
+    # python -m models.sentencepiece.tokenizer_parallel -i
