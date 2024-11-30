4ef2b8e12be8c34a4d98e2d5ba9d418c93e48aef
Hansimov <591172499@qq.com>
2024-12-01 01:39:48 +0800

    :gem: [Feature] SentenceTokenizerBenchmarker: rename load_model to init_tokenizer_params, put model instanization in benchmark, and moduralize log_epoch_stats and log_total_stats

--- models/sentencepiece/benchmark.py
+++ models/sentencepiece/benchmark.py
@@ -2,11 +2,13 @@ import argparse
 import sys
 import timeit
 
+from collections.abc import Generator
 from pathlib import Path
-from tclogger import logger
+from tclogger import logger, dict_to_str
 
 # from models.sentencepiece.tokenizerC import SentenceFullTokenizer
 from models.sentencepiece.tokenizer import SentenceFullTokenizer
+from models.sentencepiece.tokenizer_parallel import ParallelSentenceFullTokenizer
 from models.sentencepiece.test import TEST_SENTENCES
 
 
@@ -16,46 +18,57 @@ class SentenceTokenzierBenchmarker:
     ):
         self.model_prefix = model_prefix
         self.drop_non_word = drop_non_word
-        self.load_model()
+        self.init_tokenizer_params()
 
-    def load_model(self):
-        logger.note("> Loading tokenzier ...")
+    def init_tokenizer_params(self):
+        logger.note("> Init tokenzier params ...")
         self.model_path = str(Path(f"{self.model_prefix}.model").resolve())
-        logger.file(f"  * {self.model_path}")
-        self.tokenizer = SentenceFullTokenizer(
-            self.model_path, drop_non_word=self.drop_non_word, verbose=True
-        )
+        self.tokenizer_params = {
+            "model_path": self.model_path,
+            "drop_non_word": self.drop_non_word,
+            "verbose": True,
+        }
+        logger.mesg(dict_to_str(self.tokenizer_params), indent=2)
 
     def test(self):
+        self.tokenizer = SentenceFullTokenizer(**self.tokenizer_params)
         logger.note("> Testing ...")
         for sentence in TEST_SENTENCES[:]:
             tokens = self.tokenizer.tokenize(sentence)
             pretty_tokens = self.tokenizer.stringify(tokens)
             logger.mesg(f"  * {pretty_tokens}")
 
-    def benchmark(self):
-        logger.note("> Benchmarking ...")
-        sum_time = 0
-        epochs, iterations = 3, 10000
-        test_sentence = TEST_SENTENCES[-1]
-        test_sentence_len = len(test_sentence)
-        for i in range(epochs):
-            res = timeit.timeit(
-                lambda: self.tokenizer.tokenize(test_sentence), number=iterations
-            )
-            iter_per_sec = iterations / res
-            chars_per_sec = int(test_sentence_len * iter_per_sec)
-            logger.file(
-                f"  * {res:.2f}s: {iter_per_sec:.1f} iter/s; {chars_per_sec} char/s"
-            )
-            sum_time += res
+    def log_epoch_stats(
+        self, epoch_time: float, epochs: int, iterations: int, test_sentence_len: int
+    ):
+        iter_per_sec = iterations / epoch_time
+        chars_per_sec = int(test_sentence_len * iter_per_sec)
+        logger.file(
+            f"  * {epoch_time:.2f}s: {iter_per_sec:.1f} iter/s; {chars_per_sec} char/s"
+        )
 
-        avg_time = sum_time / epochs
+    def log_total_stats(
+        self, total_time: float, epochs: int, iterations: int, test_sentence_len: int
+    ):
+        avg_time = total_time / epochs
         avg_iter_per_sec = iterations / avg_time
         avt_chars_per_sec = int(test_sentence_len * avg_iter_per_sec)
         logger.success(
             f"{avg_time:.2f}s: {avg_iter_per_sec:.1f} iter/s; {avt_chars_per_sec} char/s"
         )
+
+    def benchmark(self, epochs: int = 5, iterations: int = 100000):
+        logger.note("> Benchmarking ...")
+        self.tokenizer = SentenceFullTokenizer(**self.tokenizer_params)
+        test_sentence = TEST_SENTENCES[-1]
+        test_sentence_len = len(test_sentence)
+        total_time = 0
+        func = self.tokenizer.tokenize
+        for i in range(epochs):
+            epoch_time = timeit.timeit(lambda: func(test_sentence), number=iterations)
+            self.log_epoch_stats(epoch_time, epochs, iterations, test_sentence_len)
+            total_time += epoch_time
+        self.log_total_stats(total_time, epochs, iterations, test_sentence_len)
 
 
 class ArgParser(argparse.ArgumentParser):
