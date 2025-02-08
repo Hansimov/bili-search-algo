14b21cab8cea3226fdf0abbb947aea1b38582220
Hansimov <591172499@qq.com>
2025-02-08 11:16:27 +0800

    :gem: [Feature] New MilvusVideoSchemaCreator: create schema for bvid, docvec, status and array

--- workers/milvus_videos/schema.py
+++ workers/milvus_videos/schema.py
@@ -8,13 +8,12 @@ In-memory Index | Milvus Documentation
   * This doc describes suitable indexes in different scenarios.
 """
 
-DOCVEC_DIM = FasttextDocVecModelRunner.docvec_dim
-
 DEFAULT_SCHEMA_PARAMS = {
     "auto_id": False,
     "enable_dynamic_field": True,
 }
 
+DOCVEC_DIM = FasttextDocVecModelRunner.docvec_dim
 DEFAULT_DOCVEC_SCHEMA_PARAMS = {
     "datatype": DataType.FLOAT16_VECTOR,
     "dim": DOCVEC_DIM,
@@ -28,31 +27,83 @@ DEFAULT_VECTOR_INDEX_PARAMS = {
     },
 }
 
-DOCVEC_COLUMNS = ["title", "desc", "tags", "owner", "title_tags_desc_owner"]
+DOCVEC_COLUMNS = [
+    *["title"],
+    *["tags"],
+    *["title_tags_owner", "title_tags_owner_desc"],
+]
+ARRAY_COLUMNS = ["stats"]
+STATUS_COLUMNS = ["title", "tags"]
 
 
-def create_docvec_schema_params(columns: list = DOCVEC_COLUMNS):
-    return {
-        f"{col}_vec": {
-            **DEFAULT_DOCVEC_SCHEMA_PARAMS,
-            "index_params": {
-                "index_name": f"{col}_vec_index",
-                **DEFAULT_VECTOR_INDEX_PARAMS,
-            },
+class MilvusVideoSchemaCreator:
+    @staticmethod
+    def create_bvid_schema() -> dict[str, dict]:
+        return {
+            "bvid": {
+                "datatype": DataType.VARCHAR,
+                "is_primary": True,
+                "max_length": 128,
+                "index_params": {
+                    "index_name": "bvid_index",
+                    "index_type": "AUTOINDEX",
+                },
+            }
         }
-        for col in columns
-    }
+
+    @staticmethod
+    def create_docvec_schemas(columns: list = DOCVEC_COLUMNS) -> dict[str, dict]:
+        return {
+            f"{col}_vec": {
+                **DEFAULT_DOCVEC_SCHEMA_PARAMS,
+                "index_params": {
+                    "index_name": f"{col}_vec_index",
+                    **DEFAULT_VECTOR_INDEX_PARAMS,
+                },
+            }
+            for col in columns
+        }
+
+    @staticmethod
+    def create_status_schemas(columns: list = STATUS_COLUMNS) -> dict[str, dict]:
+        """Flag status of related fields.
+        For example, `title_status` is used to indicate whether title text is embedded as `title_vec`,
+        and `tags_status` is used to indicate whether tags-related fields (such as `tags`, `title_tags_owner`, `title_tags_owner_desc`) are embedded.
+        """
+        return {
+            f"{col}_status": {
+                "datatype": DataType.INT8,
+                "index_params": {
+                    "index_name": f"{col}_status_index",
+                    "index_type": "BITMAP",
+                },
+            }
+            for col in columns
+        }
+
+    @staticmethod
+    def create_array_schemas(columns: list = ARRAY_COLUMNS) -> dict[str, dict]:
+        return {
+            f"{col}_array": {
+                "datatype": DataType.ARRAY,
+                "element_type": DataType.INT64,
+                "max_capacity": 16,
+                "index_params": {
+                    "index_name": f"{col}_array_index",
+                    "index_type": "AUTOINDEX",
+                },
+            }
+            for col in columns
+        }
+
+    @staticmethod
+    def create_schema() -> dict[str, dict]:
+        return {
+            **MilvusVideoSchemaCreator.create_bvid_schema(),
+            **MilvusVideoSchemaCreator.create_docvec_schemas(),
+            **MilvusVideoSchemaCreator.create_status_schemas(),
+            **MilvusVideoSchemaCreator.create_array_schemas(),
+        }
 
 
-MILVUS_VIDEOS_COLUMNS_SCHEMA = {
-    "bvid": {
-        "datatype": DataType.VARCHAR,
-        "is_primary": True,
-        "max_length": 128,
-        "index_params": {
-            "index_type": "",
-            "index_name": "bvid_index",
-        },
-    },
-    **create_docvec_schema_params(),
-}
+MILVUS_VIDEOS_COLUMNS_SCHEMA = MilvusVideoSchemaCreator.create_schema()
