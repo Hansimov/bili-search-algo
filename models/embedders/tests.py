2b438bd9ba029951c19dc0b4563de9bc90e7fb37
Hansimov <591172499@qq.com>
2025-05-01 18:15:04 +0800

    :zap: [Enhance] SentfmEmbedder: add backend and model_kwargs with onnx, and enable use prefix for test_embedder

--- models/embedders/tests.py
+++ models/embedders/tests.py
@@ -1,28 +1,44 @@
 from tclogger import logger, logstr
+from typing import Union
 
 from models.fasttext.test import TEST_PAIRS
 from models.vectors.calcs import dot_sim
 
 
-def test_embedder(embedder):
+class EmbedderBase:
+    def embed(self, sentences: Union[str, list[str]]):
+        raise NotImplementedError("Please implement embed() in the subclass.")
+
+
+def add_prefix(text: str, prefix: str = "") -> str:
+    if prefix:
+        return f"{prefix} {text}"
+    else:
+        return text
+
+
+def test_embedder(
+    embedder: EmbedderBase, query_prefix: str = None, passage_prefix: str = None
+):
     for pair in TEST_PAIRS:
         query = pair[0]
         samples = pair[1]
         if isinstance(query, list):
             query = " ".join(query)
+        logger.note(f"  * [{logstr.file(query)}]: ")
+        query = add_prefix(query, prefix=query_prefix)
         query_vector = embedder.embed(query)
         sample_vectors = []
         scores = []
         for sample in samples:
             if isinstance(sample, list):
                 sample = " ".join(sample)
+            sample = add_prefix(sample, prefix=passage_prefix)
             sample_vector = embedder.embed(sample)
             sample_vectors.append(sample_vector)
             score = dot_sim(query_vector, sample_vector, 4)
             scores.append(score)
         sample_scores = list(zip(samples, scores))
         sample_scores.sort(key=lambda x: x[-1], reverse=True)
-
-        logger.note(f"  * [{logstr.file(query)}]: ")
         for sample, score in sample_scores:
             logger.success(f"    * {score:>.4f}: {logstr.file(sample)}")
--- models/sentfm/embed.py
+++ models/sentfm/embed.py
@@ -1,33 +1,75 @@
 import numpy as np
 
 from sentence_transformers import SentenceTransformer
-from tclogger import logger, logstr, dict_to_str, brk, brp
-from typing import Literal, Union
+from tclogger import logger, logstr, brk
+from typing import Union, Literal
+
+"""
+```sh
+pip install --upgrade torch torchvision
+pip install --upgrade tensorrt
+pip install --upgrade 'sentence-transformers[onnx-gpu]'
+```
+"""
 
 
 class SentfmEmbedder:
     def __init__(
         self,
         model_name: str = "BAAI/bge-base-zh-v1.5",
+        backend: Literal["torch", "onnx"] = "torch",
+        model_kwargs: dict = None,
         verbose: bool = False,
     ):
         self.model_name = model_name
+        self.backend = backend
+        self.model_kwargs = model_kwargs or None
         self.verbose = verbose
 
     def load_model(self):
         if self.verbose:
             logger.note(f"> Loading model: {logstr.file(brk(self.model_name))}")
-        self.model = SentenceTransformer(self.model_name)
+        self.model = SentenceTransformer(
+            self.model_name, backend=self.backend, model_kwargs=self.model_kwargs
+        )
 
     def embed(self, sentences: Union[str, list[str]]) -> np.ndarray:
         return self.model.encode(sentences)
 
 
+def test_bge_zh():
+    # model_name = "BAAI/bge-base-zh-v1.5"
+
+    # model_name = "BAAI/bge-large-zh-v1.5"
+    # embedder = SentfmEmbedder(model_name, verbose=True)
+
+    model_name = "Xenova/bge-large-zh-v1.5"
+    backend = "onnx"
+    model_kwargs = {"file_name": "model_fp16.onnx"}
+    embedder = SentfmEmbedder(
+        model_name, verbose=True, backend=backend, model_kwargs=model_kwargs
+    )
+
+    embedder.load_model()
+    test_embedder(
+        embedder,
+        # query_prefix="为这个句子生成表示以用于检索相关文章：",
+        query_prefix="",
+        passage_prefix="",
+    )
+
+
+def test_e5():
+    model_name = "intfloat/multilingual-e5-large"
+    embedder = SentfmEmbedder(model_name, verbose=True)
+    embedder.load_model()
+    test_embedder(embedder, query_prefix="query:", passage_prefix="passage:")
+
+
 if __name__ == "__main__":
     from models.embedders.tests import test_embedder
 
-    embedder = SentfmEmbedder("BAAI/bge-base-zh-v1.5", verbose=True)
-    embedder.load_model()
-    test_embedder(embedder)
+    # test_e5()
+    test_bge_zh()
 
     # python -m models.sentfm.embed
