895bd505200a6cae854ef2265cbc687c50c2781d
Hansimov <591172499@qq.com>
2025-08-12 17:17:08 +0800

    :zap: [Enhance] HFTransformersEmbedder: add set_attention_prune with resize_linear_layer, and rename original prune to prune_layer

--- models/hftfm/embed.py
+++ models/hftfm/embed.py
@@ -10,6 +10,38 @@ from typing import Literal
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
 
+def resize_linear_layer(
+    linear_layer: torch.nn.Linear,
+    new_out_features: int = None,
+    new_in_features: int = None,
+):
+    old_weight = linear_layer.weight.data
+    old_bias = linear_layer.bias.data if linear_layer.bias is not None else None
+
+    if new_in_features is None:
+        new_in_features = linear_layer.in_features
+
+    new_linear_layer = torch.nn.Linear(
+        new_in_features,
+        new_out_features,
+        bias=linear_layer.bias is not None,
+        device=linear_layer.weight.device,
+        dtype=linear_layer.weight.dtype,
+    )
+
+    weight_out_size = min(new_out_features, old_weight.size(0))
+    weight_in_size = min(new_in_features, old_weight.size(1))
+
+    new_linear_layer.weight.data = old_weight[:weight_out_size, :weight_in_size].clone()
+
+    if old_bias is not None:
+        new_linear_layer.bias.data = old_bias[:weight_out_size].clone()
+
+    new_linear_layer = new_linear_layer.to(linear_layer.weight.device)
+
+    return new_linear_layer
+
+
 class HFTransformersEmbedder:
     def __init__(
         self,
@@ -17,17 +49,21 @@ class HFTransformersEmbedder:
         model_path: PathType = None,
         device: Literal["cuda", "cpu"] = "cuda",
         use_quantize: bool = False,
-        use_prune: bool = False,
-        prune_ratio: float = 0.75,
+        use_layer_prune: bool = False,
+        layer_prune_ratio: float = 0.75,
+        use_attention_prune: bool = False,
+        attention_prune_ratio: float = 0.75,
         model_kwargs: dict = None,
         verbose: bool = False,
     ):
         self.model_name = model_name
         self.model_path = model_path
         self.device = device
-        self.use_prune = use_prune
-        self.prune_ratio = prune_ratio
         self.use_quantize = use_quantize
+        self.use_layer_prune = use_layer_prune
+        self.layer_prune_ratio = layer_prune_ratio
+        self.use_attention_prune = use_attention_prune
+        self.attention_prune_ratio = attention_prune_ratio
         self.model_kwargs = model_kwargs or None
         self.verbose = verbose
 
@@ -62,34 +98,81 @@ class HFTransformersEmbedder:
             "low_cpu_mem_usage": self.low_cpu_mem_usage,
         }
 
-    def set_prune(self):
-        if (
-            self.use_prune
-            and hasattr(self.model, "encoder")
-            and hasattr(self.model.encoder, "layer")
-        ):
-            old_layers = len(self.model.encoder.layer)
-            if self.prune_ratio:
-                new_layers = max(1, int(old_layers * self.prune_ratio))
-            else:
-                new_layers = old_layers
-            if new_layers < old_layers:
-                self.model.encoder.layer = self.model.encoder.layer[-new_layers:]
+    def has_layer(self) -> bool:
+        return hasattr(self.model, "encoder") and hasattr(self.model.encoder, "layer")
 
-            if self.verbose:
-                if new_layers < old_layers:
-                    layers_str = f"{old_layers} -> {new_layers}"
-                else:
-                    layers_str = f"{new_layers}"
-                logger.mesg(f"  * layers : {logstr.file(layers_str)}")
+    def set_layer_prune(self):
+        if not (self.use_layer_prune and self.has_layer()):
+            return
+        if not self.layer_prune_ratio:
+            return
+        old_layers = len(self.model.encoder.layer)
+        new_layers = max(1, int(old_layers * self.layer_prune_ratio))
+        if new_layers < old_layers:
+            self.model.encoder.layer = self.model.encoder.layer[-new_layers:]
+
+        if self.verbose:
+            if new_layers < old_layers:
+                layers_str = f"{old_layers} -> {new_layers}"
+            else:
+                layers_str = f"{new_layers}"
+            logger.mesg(f"  * layers: {logstr.file(layers_str)}")
+
+    def has_attention(self, layer: torch.nn.Module) -> bool:
+        return hasattr(layer, "attention") and hasattr(layer.attention, "self")
+
+    def set_attention_prune(self):
+        if not (self.use_attention_prune and self.has_layer()):
+            return
+        if not self.attention_prune_ratio:
+            return
+
+        for layer in self.model.encoder.layer:
+            if not self.has_attention(layer):
+                continue
+
+            old_heads = layer.attention.self.num_attention_heads
+            new_heads = max(1, int(old_heads * self.attention_prune_ratio))
+
+            if new_heads < old_heads:
+                head_size = layer.attention.self.attention_head_size
+                new_all_head_size = new_heads * head_size
+
+                # adjust attention layers to new number of heads
+                layer.attention.self.num_attention_heads = new_heads
+                layer.attention.self.all_head_size = new_all_head_size
+
+                # adjust Q, K, V layers to new all_head_size
+                layer.attention.self.query = resize_linear_layer(
+                    layer.attention.self.query, new_all_head_size
+                )
+                layer.attention.self.key = resize_linear_layer(
+                    layer.attention.self.key, new_all_head_size
+                )
+                layer.attention.self.value = resize_linear_layer(
+                    layer.attention.self.value, new_all_head_size
+                )
+
+                # adjust new input dim to new all_head_size,
+                # keeps output dim as old hidden_size
+                old_hidden_size = layer.attention.output.dense.out_features
+                layer.attention.output.dense = resize_linear_layer(
+                    layer.attention.output.dense,
+                    old_hidden_size,
+                    new_all_head_size,
+                )
+
+                if self.verbose:
+                    heads_str = f"{old_heads} -> {new_heads}"
+                    logger.mesg(f"    * attention heads: {logstr.file(heads_str)}")
 
     def log_model_loading(self):
         if self.verbose:
             logger.note(f"> Loading model:")
             if self.model_name:
-                logger.mesg(f"  * model_name  : {logstr.file(self.model_name)}")
+                logger.mesg(f"  * model_name: {logstr.file(self.model_name)}")
             if self.model_path:
-                logger.mesg(f"  * model_path  : {logstr.file(self.model_path)}")
+                logger.mesg(f"  * model_path: {logstr.file(self.model_path)}")
 
     def log_model_loaded(self):
         if self.verbose:
@@ -109,14 +192,14 @@ class HFTransformersEmbedder:
         self.tokenizer = AutoTokenizer.from_pretrained(
             model_name_or_path, use_fast=True
         )
-
         self.model = AutoModel.from_pretrained(
             model_name_or_path,
             **self.device_kwargs,
             **self.quantize_kwargs,
             **model_kwargs,
         )
-        self.set_prune()
+        self.set_layer_prune()
+        self.set_attention_prune()
         self.model.eval()
 
         self.log_model_loaded()
@@ -149,8 +232,10 @@ def test_hftfm_bge_zh():
         model_path=model_path,
         device="cpu",
         use_quantize=False,
-        use_prune=True,
-        prune_ratio=0.5,
+        use_layer_prune=True,
+        layer_prune_ratio=0.5,
+        use_attention_prune=True,
+        attention_prune_ratio=0.5,
         model_kwargs=model_kwargs,
         verbose=True,
     )
