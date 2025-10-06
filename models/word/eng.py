d210eecb4c651722d950b79667223d80c333cba8
Hansimov <591172499@qq.com>
2025-10-07 00:48:48 +0800

    :gem: [Feature] models.eng: rename to EnglishWordsRecorder to avoid ambiguity, and implement record with update_record_by_doc/term and filter/sort_records

--- models/word/eng.py
+++ models/word/eng.py
@@ -1,7 +1,9 @@
 import re
 
+from itertools import islice
 from sedb import MongoDocsGenerator
-from tclogger import logger, dict_get, dict_to_str
+from tclogger import logger, logstr, dict_get
+from typing import TypedDict
 
 from configs.envs import MONGO_ENVS
 
@@ -39,30 +41,119 @@ REP_ENG = re.compile(RE_ENG, re.VERBOSE)
 
 class EnglishWordExtractor:
     def extract(self, text: str) -> list[str]:
-        matches = REP_ENG.findall(text)
-        return matches
+        return REP_ENG.findall(text)
 
 
-class EnglishWordsCounter:
-    def __init__(self, generator: MongoDocsGenerator):
+class RecordType(TypedDict):
+    word: str
+    doc_freq: int
+    term_freq: int
+    first_seen: int
+    last_seen: int
+
+
+TEXT_FIELDS = ["title", "tags", "desc"]
+
+
+class EnglishWordsRecorder:
+    def __init__(
+        self,
+        generator: MongoDocsGenerator,
+        text_fields: list[str] = TEXT_FIELDS,
+        sort_key: str = "doc_freq",
+        min_count: int = 2,
+    ):
         self.generator = generator
+        self.text_fields = text_fields
+        self.sort_key = sort_key
+        self.min_count = min_count
         self.extractor = EnglishWordExtractor()
+        self.records: dict[str, RecordType] = {}
+
+    def doc_to_text(self, doc: dict):
+        text = ""
+        for field in self.text_fields:
+            field_str = dict_get(doc, field)
+            if field_str == "-":
+                # speed-up for desc
+                continue
+            if field_str:
+                if text:
+                    text += " | " + field_str
+                else:
+                    text = field_str
+        return text.strip().lower()
+
+    def update_record_by_doc(self, word: str, doc_idx: int):
+        if word not in self.records:
+            self.records[word] = {
+                "word": word,
+                "doc_freq": 1,
+                "term_freq": 0,
+                "first_seen": doc_idx,
+                "last_seen": doc_idx,
+            }
+        last_seen = self.records[word]["last_seen"]
+        if last_seen != doc_idx:
+            self.records[word]["doc_freq"] += 1
+            self.records[word]["last_seen"] = doc_idx
+
+    def update_record_by_term(self, word: str):
+        self.records[word]["term_freq"] += 1
+
+    def sort_records(self, reverse: bool = True):
+        self.records = dict(
+            sorted(
+                self.records.items(),
+                key=lambda item: item[1][self.sort_key],
+                reverse=reverse,
+            )
+        )
+
+    def filter_records(self):
+        logger.note(f"> Filter records:")
+        logger.mesg(f"  * {self.sort_key} < {self.min_count}")
+        old_count = len(self.records)
+        self.records = {
+            word: record
+            for word, record in self.records.items()
+            if record[self.sort_key] >= self.min_count
+        }
+        new_count = len(self.records)
+        diff_count = old_count - new_count
+        if old_count > 0:
+            diff_ratio = diff_count / old_count * 100
+        else:
+            diff_ratio = 0
+        count_str = f"{logstr.okay(new_count)}/{logstr.warn(diff_count)}/{logstr.mesg(old_count)}"
+        ratio_str = logstr.warn(f"-{diff_ratio:.1f}%")
+        logger.okay(f"  * {count_str} ({ratio_str})")
+
+    def log_results(self):
+        top_k = 15
+        total_n = len(self.records)
+        count_str = f"{logstr.file(top_k)}/{logstr.mesg(total_n)}"
+        logger.note(f"> Top words: ({count_str})")
+        for i, v in enumerate(islice(self.records.values(), top_k)):
+            print(f"[{i}]: {v}")
 
     def run(self):
-        for doc in self.generator.doc_generator():
-            # logger.line(doc)
-            text = ""
-            for field in ["title", "tags"]:
-                field_str = dict_get(doc, field)
-                if field_str:
-                    if text:
-                        text += " | " + doc[field]
-                    else:
-                        text = doc[field]
-            if text:
-                eng_words = self.extractor.extract(text)
-                if eng_words:
-                    logger.okay(eng_words)
+        for doc_idx, doc in enumerate(self.generator.doc_generator()):
+            text = self.doc_to_text(doc)
+            if not text:
+                continue
+            words = self.extractor.extract(text)
+            # if words:
+            #     logger.okay(f"[{doc_idx}]: {words}")
+            unique_words = set(words)
+            for w in unique_words:
+                self.update_record_by_doc(w, doc_idx)
+            for w in words:
+                self.update_record_by_term(w)
+        print()
+        self.filter_records()
+        self.sort_records()
+        self.log_results()
 
 
 def main():
@@ -71,14 +162,15 @@ def main():
         ikvs={
             **MONGO_ENVS,
             "mongo_collection": "videos",
-            "include_fields": "title,tags",
-            "extra_filters": "u:stat.view>1w;d:pubdate>=2025-10-05",
+            "include_fields": "title,tags,desc",
+            "extra_filters": "u:stat.view>1k;d:pubdate>=2025-09-01",
         }
     )
     logger.okay(generator.args)
-    generator.init_all_with_cli_args(set_count=False, set_bar=False)
-    counter = EnglishWordsCounter(generator)
-    counter.run()
+    # generator.init_all_with_cli_args(set_count=False, set_bar=False)
+    generator.init_all_with_cli_args()
+    recorder = EnglishWordsRecorder(generator)
+    recorder.run()
 
 
 if __name__ == "__main__":
