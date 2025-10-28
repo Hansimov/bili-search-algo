cbe5255fc908584c7a487ad94217663fbc1262b8
Hansimov <591172499@qq.com>
2025-10-29 03:47:22 +0800

    :gem: [Feature] models.tembed.train: New TextEmbedExamineDataGenerator with enabling elastic search request, and add TembedTrainerArgParser for task switch

--- models/tembed/train.py
+++ models/tembed/train.py
@@ -1,22 +1,27 @@
+import argparse
 import random
 import polars as pl
 
 from pathlib import Path
 from sedb import ElasticOperator
-from tclogger import logger, logstr
+from tclogger import logger, logstr, brk, dict_to_str
 from typing import Literal
 
 from models.word.eng import get_dump_path
+from configs.envs import SECRETS
 
 
 class QuerySampler:
     """Sample queries for query-passage pairs."""
 
+    def __init__(self):
+        self.save_path = Path(__file__).parent / "keyword_samples.csv"
+
     def load_csv(self, docs_count: int = None, lang: Literal["zh", "en"] = None):
         logger.note(f"> Loading csv from:")
-        csv_path = get_dump_path(docs_count=docs_count, lang=lang)
-        self.df = pl.read_csv(csv_path)
-        logger.okay(f"  * {csv_path}")
+        freq_path = get_dump_path(docs_count=docs_count, lang=lang)
+        self.df = pl.read_csv(freq_path)
+        logger.okay(f"  * {freq_path}")
         logger.line(self.df, indent=4)
 
     def random_samples(self, num: int = 100, seed: int = None):
@@ -39,25 +44,75 @@ class QuerySampler:
 
     def save_csv(self):
         logger.note(f"> Saving samples to csv:")
-        save_path = Path(__file__).parent / "keyword_samples.csv"
-        self.samples.write_csv(save_path)
-        logger.okay(f"  * {save_path}")
+        self.samples.write_csv(self.save_path)
+        logger.okay(f"  * {self.save_path}")
 
 
 class TextEmbedExamineDataGenerator:
     """Generate examine data for text embedding model tuning."""
 
-    pass
+    def __init__(self):
+        self.configs = SECRETS["elastic_dev"]
+        self.index_name = "bili_videos_dev5"
+        self.es = ElasticOperator(configs=self.configs, connect_cls=self.__class__)
+
+    def query_to_es_dict(self, query: str) -> dict:
+        query_dict = {
+            "query": {
+                "es_tok_query_string": {
+                    "query": query,
+                    "type": "cross_fields",
+                    "fields": [
+                        "title.words^3",
+                        "tags.words^2.5",
+                        "owner.name.words^2",
+                        "desc.words^0.1",
+                    ],
+                    "max_freq": 1000000,
+                    "min_kept_tokens_count": 2,
+                    "min_kept_tokens_ratio": -1,
+                }
+            },
+            "_source": ["title", "tags", "owner.name", "desc", "stat.favorite"],
+            "size": 10,
+        }
+        return query_dict
+
+    def search(self, query: str) -> dict:
+        query_dict = self.query_to_es_dict(query)
+        res = self.es.client.search(index=self.index_name, body=query_dict)
+        return res
+
+
+class TembedTrainerArgParser(argparse.ArgumentParser):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.add_argument("-s", "--sample", action="store_true")
+        self.add_argument("-g", "--generate", action="store_true")
+        self.args = self.parse_args()
 
 
 def main():
-    sampler = QuerySampler()
-    sampler.load_csv(docs_count=770000000, lang="zh")
-    sampler.random_samples(num=10000, seed=2)
-    sampler.save_csv()
+    arg_parser = TembedTrainerArgParser()
+    args = arg_parser.args
+
+    if args.sample:
+        sampler = QuerySampler()
+        sampler.load_csv(docs_count=770000000, lang="zh")
+        sampler.random_samples(num=10000, seed=2)
+        sampler.save_csv()
+
+    if args.generate:
+        generator = TextEmbedExamineDataGenerator()
+        queries = ["机器学习"]
+        for query in queries:
+            logger.note(f"> Query: {logstr.mesg(brk(query))}")
+            res = generator.search(query)
+            logger.line(dict_to_str(res), indent=4)
 
 
 if __name__ == "__main__":
     main()
 
-    # python -m models.tembed.train
+    # python -m models.tembed.train -s
+    # python -m models.tembed.train -g
